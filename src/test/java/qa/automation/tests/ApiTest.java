7075808ba035e820a9f13fe7b4181278e265f816
johnson_phillips <johnson.phillips@mheducation.com>
2019-07-10 17:12:14 -0400

    updated test case and added compiler version to pom

--- src/test/java/qa/automation/tests/ApiTest.java
+++ src/test/java/qa/automation/tests/ApiTest.java
@@ -3,6 +3,10 @@ package qa.automation.tests;
 import org.testng.annotations.Test;
 import qa.automation.core.Api;
 import qa.automation.report.TestData;
+import java.util.UUID;
+
+import static io.restassured.RestAssured.given;
+import static qa.automation.core.Api.readResponse;
 
 public class ApiTest {
 
@@ -11,7 +15,7 @@ public class ApiTest {
     {
         TestData.startTest("first test",1);
         Api.printconsole = true;
-        Api.getData("http://dummy.restapiexample.com/api/v1/employees");
+        readResponse(given().body("{\"name\":\""+ UUID.randomUUID().toString() +"\",\"salary\":\"123\",\"age\":\"23\"}").post("http://dummy.restapiexample.com/api/v1/create"));
         TestData.endTest();
     }
 }
